fe612651021214c1ccd01140f5aeb04eea727b3b
Marcel <MTRNord@users.noreply.github.com>
2016-10-10 10:17:37 +0200

    Update createWork_ProjectStreet.py

--- bin/createWork_ProjectStreet.py
+++ bin/createWork_ProjectStreet.py
@@ -4,13 +4,17 @@ import os.path
 import time
 from subprocess import call
 import tarfile
+import zstandard
 ### Need to add  way to find job
+
+dctx = zstd.ZstdDecompressor()
 
 while not os.path.exists(file_path):
     time.sleep(1)
 
 if os.path.isfile(file_path):
     # read file
-    call(["ls", "-l"])
+    call(["ls", "-l", file_path])
+    decompressed = dctx.decompress(data)
 else:
     raise ValueError("%s isn't a file!" % file_path)
