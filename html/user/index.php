97a7731bf77d574facb22d154a4b764ad5d287fb
Marcel <MTRNord@users.noreply.github.com>
2016-10-11 22:28:23 +0200

    fix tag

--- html/user/index.php
+++ html/user/index.php
@@ -52,7 +52,7 @@ echo <<<END
     <![endif]-->
 	  </div>
 	  <div id="feature">
-	     <sh2><img src="img/concept_icon.png" alt="concept" />FEATURED CONCEPT: Dark Matter</h2>
+	     <h2><img src="img/concept_icon.png" alt="concept" />FEATURED CONCEPT: Dark Matter</h2>
      <div class="featuretext">
      Unlike ordinary matter, dark matter does not emit or absorb light--or any other type of electromagnetic radiation. Consequently, dark matter cannot be observed directly using a telescope or any other astronomical instrument that has been developed by humans. If dark matter has these strange properties, how do we know that it exists in the first place?
      <br> 
