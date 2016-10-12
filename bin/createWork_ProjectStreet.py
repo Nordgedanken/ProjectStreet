b55b6b91fb29d37208af7f0e74b48e7bf3c60e03
Marcel <MTRNord@users.noreply.github.com>
2016-10-12 21:49:23 +0200

    Add Arguments and Fix some Generation

--- bin/createWork_ProjectStreet.py
+++ bin/createWork_ProjectStreet.py
@@ -2,36 +2,44 @@
 from Boinc import boinc_project_path
 import os
 import docker
-import random
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--last_wu_results', type=string,
+                    help='Path to the resultds of the last wu')
+parser.add_argument('--last_wu_id', type=string,
+                    help='last wu_ID')
+parser.add_argument('--last_stage', type=string,
+                    help='last wu_stage')
+
+args = parser.parse_args()
 cli = docker.Client(base_url='unix://var/run/docker.sock')
 RawData = os.path.join(boinc_project_path, 'rawData')
 boinc2docker = os.path.join(boinc_project_path, 'bin', 'boinc2docker_create_work.py')
 def analyse():
     if os.listdir(RawData):
-            files = sorted(os.listdir(RawData), key=os.path.getctime)
-            oldest = files[0]
-            last_wu_results = args.last_wu_results  #needs to be as []
-            last_wu_id = args.last_wu_id
-            last_stage = args.last_stage
-            hash = random.getrandbits(10)
-            if last_stage:
-                if last_stage == "1"
-                    addFiles(last_wu_results, last_wu_id)
-                    os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "2" >> /root/shared/results/stage.txt && ./stage2_generateHaarDetector.sh 2>&1 | tee /root/shared/results/logs.txt"')
-                if last_stage == "2"
-                    addFiles(last_wu_results, last_wu_id)
-                    os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "3" >> /root/shared/results/stage.txt && ./stage3_analyseVideo.py 2>&1 | tee /root/shared/results/logs.txt"')
-            else:
-                addFiles(None, hash, os.path.join(RawData, oldest))
-                os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "1" >> /root/shared/results/stage.txt && ./stage2_generateHaarDetector.sh 2>&1 | tee /root/shared/results/logs.txt"')
-    	        if not os.path.exists(os.path.dirname(os.path.join(RawData, 'old'))):
-                    try:
-                        os.makedirs(os.path.dirname(os.path.join(RawData, 'old')))
-                    except OSError as exc: # Guard against race condition
-                        if exc.errno != errno.EEXIST:
-                            raise
-                    os.rename(os.path.join(RawData, oldest), os.path.join(RawData, "old", oldest))
+        files = sorted(os.listdir(RawData), key=os.path.getctime)
+        oldest = files[0]
+        last_wu_results = args.last_wu_results
+        last_wu_id = args.last_wu_id
+        last_stage = args.last_stage
+        if last_stage:
+            if last_stage == "1"
+                addFiles([last_wu_results], last_wu_id)
+                os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' sh -c "echo "2" >> /root/shared/results/stage.txt && ./stage2_generateHaarDetector.sh 2>&1 | tee /root/shared/results/logs.txt"')
+            if last_stage == "2"
+                addFiles([last_wu_results], last_wu_id)
+                addFiles(None, last_wu_id, os.path.join(RawData, oldest))
+                os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' sh -c "echo "3" >> /root/shared/results/stage.txt && ./stage3_analyseVideo.py 2>&1 | tee /root/shared/results/logs.txt"')
+                if not os.path.exists(os.path.dirname(os.path.join(RawData, 'old'))):
+                try:
+                    os.makedirs(os.path.dirname(os.path.join(RawData, 'old')))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+                os.rename(os.path.join(RawData, oldest), os.path.join(RawData, "old", oldest))
+        else:
+            os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:latest sh -c "echo "1" >> /root/shared/results/stage.txt && ./stage1_getNeg.sh 2>&1 | tee /root/shared/results/logs.txt"')
                     
                     
 def make_tree(dirs, files):
