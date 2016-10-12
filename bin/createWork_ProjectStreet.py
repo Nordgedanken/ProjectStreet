1dab91e39011becf7dbbefc6107abe571b5297cf
Marcel <MTRNord@users.noreply.github.com>
2016-10-12 20:47:37 +0200

    Some fixes

--- bin/createWork_ProjectStreet.py
+++ bin/createWork_ProjectStreet.py
@@ -6,17 +6,19 @@ from docker import Client
 cli = Client(base_url='unix://var/run/docker.sock')
 RawData = os.path.join(boinc_project_path, 'rawData')
 
-if os.listdir(RawData):
+def analyse():
+    if os.listdir(RawData):
+            files = sorted(os.listdir(RawData), key=os.path.getctime)
+            oldest = files[0]
 
 
 
 
 def addFiles():
-    files = sorted(os.listdir(RawData), key=os.path.getctime)
-    oldest = files[0]
+
     try:
-      container = cli.start(image='mtrnord/projectstreet_detection:latest', command='/bin/sleep 300')
-      container_ID = container["Id"]
+        container = cli.create_container(image='mtrnord/projectstreet_detection:latest', command='/bin/sleep 300')
+        container_ID = container["Id"]
     
     else:
-      print "something wrent wrong"
+        print "something wrent wrong"
