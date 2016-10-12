6bb09650df918cdbb0ed9ba6da52e951df5e5d2c
Marcel <MTRNord@users.noreply.github.com>
2016-10-12 21:35:48 +0200

    Prepare Work Generation.
    Need to modify assimilator

--- bin/createWork_ProjectStreet.py
+++ bin/createWork_ProjectStreet.py
@@ -1,24 +1,58 @@
 ##### Generates automticly work (Run as daemon as needed)
 from Boinc import boinc_project_path
 import os
-from docker import Client
+import docker
+import random
 
-cli = Client(base_url='unix://var/run/docker.sock')
+cli = docker.Client(base_url='unix://var/run/docker.sock')
 RawData = os.path.join(boinc_project_path, 'rawData')
-
+boinc2docker = os.path.join(boinc_project_path, 'bin', 'boinc2docker_create_work.py')
 def analyse():
     if os.listdir(RawData):
             files = sorted(os.listdir(RawData), key=os.path.getctime)
             oldest = files[0]
+            last_wu_results = args.last_wu_results  #needs to be as []
+            last_wu_id = args.last_wu_id
+            last_stage = args.last_stage
+            hash = random.getrandbits(10)
+            if last_stage:
+                if last_stage == "1"
+                    addFiles(last_wu_results, last_wu_id)
+                    os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "2" >> /root/shared/results/stage.txt && ./stage2_generateHaarDetector.sh 2>&1 | tee /root/shared/results/logs.txt"')
+                if last_stage == "2"
+                    addFiles(last_wu_results, last_wu_id)
+                    os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "3" >> /root/shared/results/stage.txt && ./stage3_analyseVideo.py 2>&1 | tee /root/shared/results/logs.txt"')
+            else:
+                addFiles(None, hash, os.path.join(RawData, oldest))
+                os.system(boinc2docker + '--rsc_fpops_est 90000e10 --delay_bound 1.21e+6 mtrnord/projectstreet_detection:' + last_wu_id + ' latest sh -c "echo "1" >> /root/shared/results/stage.txt && ./stage2_generateHaarDetector.sh 2>&1 | tee /root/shared/results/logs.txt"')
+    	        if not os.path.exists(os.path.dirname(os.path.join(RawData, 'old'))):
+                    try:
+                        os.makedirs(os.path.dirname(os.path.join(RawData, 'old')))
+                    except OSError as exc: # Guard against race condition
+                        if exc.errno != errno.EEXIST:
+                            raise
+                    os.rename(os.path.join(RawData, oldest), os.path.join(RawData, "old", oldest))
+                    
+                    
+def make_tree(dirs, files):
+    #### Function comes from: https://github.com/docker/docker-py/blob/master/tests/helpers.py#L10-L20
+    base = tempfile.mkdtemp()
 
+    for path in dirs:
+        os.makedirs(os.path.join(base, path))
 
+    for path in files:
+        with open(os.path.join(base, path), 'w') as f:
+            f.write("content")
 
+    return base
 
-def addFiles():
-
+def addFiles(last_wu_results, last_wu_id, files):
     try:
-        container = cli.create_container(image='mtrnord/projectstreet_detection:latest', command='/bin/sleep 300')
-        container_ID = container["Id"]
-    
+        base = helpers.make_tree(last_wu_results, files)
+        container = cli.create_container(image='mtrnord/projectstreet_detection:latest', command='/bin/sleep 300', volumes=['/vol1'])
+        with docker.utils.tar(base) as tarFile:
+            cli.put_archive(container, '/vol1', tarFile)
+        cli.commit(container=container, tag=last_wu_id)    
     else:
         print "something wrent wrong"
